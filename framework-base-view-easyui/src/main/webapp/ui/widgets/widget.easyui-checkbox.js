afa2c5e59e3f6aae8b9eec776c060bc7c207f2db
gc <gc.centit.com>
2017-10-23 13:41:25 +0800

    checkbox和radio的布局修改,去掉各个页面上的国际化方法

--- framework-base-view-easyui/src/main/webapp/ui/widgets/widget.easyui-checkbox.js
+++ framework-base-view-easyui/src/main/webapp/ui/widgets/widget.easyui-checkbox.js
@@ -5,7 +5,7 @@ define(function (require) {
     var Core=require('core/core');
     var Mustache = require('plugins/mustache.min');
     var template = require('text!widgets/template.easyui-checkbox.html');
-    
+
     $.fn.checkbox = function () {//定义扩展组件
 
         //当该组件在一个页面出现多次时，this是一个集合，故需要通过each遍历。
@@ -13,9 +13,9 @@ define(function (require) {
         	var _obj=$(this);
     		var _key=_obj.attr('key');
     		var _classname=_obj.attr('class');
-    		var _name=_obj.attr('class');
+    		var _name=_obj.attr('name');
     		_obj.hide();
-    		
+
     		var data = DictionaryCache.get(_key);
         	// 没有数据字典缓存数据
         	if (!data) {
@@ -37,5 +37,5 @@ define(function (require) {
         	}
         });
     };
-    
+
 });
