bbfc848a168730562e29170ac2b17c648ba2ebf8
zk <zk@centit.com>
2017-10-23 13:34:03 +0800

    在首页加载 app.js 中判断工程的 contextPath

--- framework-base-view-easyui/src/main/webapp/ui/app.js
+++ framework-base-view-easyui/src/main/webapp/ui/app.js
@@ -1,25 +1,87 @@
+/**
+ *@param {string} url 完整的URL地址
+ *@returns {object} 自定义的对象
+ *@description 用法示例：var myURL = parseURL('http://abc.com:8080/dir/index.html?id=255&m=hello#top');
+ myURL.file='index.html'
+
+ myURL.hash= 'top'
+
+ myURL.host= 'abc.com'
+
+ myURL.query= '?id=255&m=hello'
+
+ myURL.params= Object = { id: 255, m: hello }
+
+ myURL.path= '/dir/index.html'
+
+ myURL.segments= Array = ['dir', 'index.html']
+
+ myURL.port= '8080'
+
+ myURL.protocol= 'http'
+
+ myURL.source= 'http://abc.com:8080/dir/index.html?id=255&m=hello#top'
+ */
+function parseURL(url) {
+  url = url || window.location.href;
+  var a = document.createElement('a');
+  a.href = url;
+  return {
+    source: url,
+    protocol: a.protocol.replace(':', ''),
+    host: a.hostname,
+    port: a.port,
+    query: a.search,
+    params: (function () {
+      var ret = {},
+        seg = a.search.replace(/^\?/, '').split('&'),
+        len = seg.length, i = 0, s;
+      for (; i < len; i++) {
+        if (!seg[i]) {
+          continue;
+        }
+        s = seg[i].split('=');
+        ret[s[0]] = s[1];
+      }
+      return ret;
+    })(),
+    file: (a.pathname.match(/\/([^\/?#]+)$/i) || [, ''])[1],
+    hash: a.hash.replace('#', ''),
+    path: a.pathname.replace(/^([^\/])/, '/$1'),
+    relative: (a.href.match(/tps?:\/\/[^\/]+(.+)/) || [, ''])[1],
+    segments: a.pathname.replace(/^\//, '').split('/')
+  };
+}
+
+(function(params) {
+  var cp = params.segments[0];
+  this.ContextPath = this.ContextPath || (cp ? ('/' + cp + '/') : '/');
+  this.ViewContextPath = this.ViewContextPath || this.ContextPath;
+  this.GLOBAL_IDENTIFY = this.GLOBAL_IDENTIFY || {};
+})(parseURL());
+
 requirejs.config({
     baseUrl: window.ViewContextPath + 'ui',
-    
+
     paths: {
 		jquery: 'js/jquery/jquery-1.11.2.min',
 
 		'ueditor': 'js/plugins/ueditor/ueditor.all',
 
 		'quill': 'js/plugins/quill/1.2.4/quill',
-		
+
 		'socket.io': 'js/plugins/socket.io-1.3.7',
-		
+
 		underscore: 'js/plugins/underscore/underscore-min',
-		
+
 		easyUI: 'js/easyui/1.5.2/jquery.easyui.min',
 
 		codeMirror: 'js/plugins/codemirror/codemirror',
-		
+
 		yepnope: 'js/plugins/yepnope/yepnope-1.5.4.min',
-		
+
 		fullcalendar: 'js/plugins/fullcalendar-3.0.1/locale/zh-cn',
-		
+
 		moment: 'js/plugins/moment.min',
 
 		uploader: 'js/plugins/uploader/uploader',
@@ -28,19 +90,19 @@ requirejs.config({
 
 		// 用户自定义参数文件路径
 		custom: '../custom',
-		
+
 		modules: '../modules',
-		
+
 		centit: 'js/centit',
-		
+
 		loaders: 'js/loaders',
-		
+
 		plugins: 'js/plugins',
-		
+
 		websocket: 'js/websocket',
 
 		myuploader: 'widgets/uploader/centit.uploader',
-		
+
         style: 'css',
 
 		locale: 'js/easyui/1.5.2/locale/easyui-lang-zh_CN',
@@ -50,7 +112,7 @@ requirejs.config({
 		fileMD5: 'js/plugins/file.md5'
 
     },
-    
+
     shim: {
     	ueditor: {
     		deps: ['plugins/ueditor/third-party/zeroclipboard/ZeroClipboard', 'plugins/ueditor/ueditor.config'],
@@ -93,7 +155,7 @@ requirejs.config({
 				'css!plugins/codemirror/codemirror.css'
 			]
 		},
-		
+
 		fullcalendar: {
 			deps: [
 			     'js/plugins/fullcalendar-3.0.1/fullcalendar.min',
@@ -101,7 +163,7 @@ requirejs.config({
 			]
 		}
     },
-    
+
     map: {
 		'*' : {
 			'css' : 'js/css.min',
