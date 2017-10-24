509cd025a90be7bcb718b4b0db985aa35a4bb4b2
zk <zk@centit.com>
2017-10-24 14:52:17 +0800

    公共 page 控制器添加方法
    $afterEvents: 从子控制器跳转回来时的事件 Map
    $getRootLayout: 获取 panel 顶级 layout 元素
    $autoHeight: 自适应高度布局

--- framework-base-view-easyui/src/main/webapp/ui/core/page.js
+++ framework-base-view-easyui/src/main/webapp/ui/core/page.js
@@ -65,42 +65,87 @@ define(function(require) {
 			this.load(panel || this.panel, data || this.data);
 		};
 
-		this.beforeLoad = function(data) {
-			return true;
-		};
-		
-		// 加载数据
-		this.load = function(panel, data) {};
-		
-		// 提交
-		this.submit = function(panel, data, closeCallback) {};
-		
-		// 注销
-		this.onClose = function() {};
-		
-		// 注销前事件
-		this.onBeforeClose = function(panel) {};
-		
-		/**
-		 * 特殊处理打印form
-		 */
-		function initPrintForm(panel) {
-			var form = panel.find('form.print');
-		    
-			if (form[0]) {
-				// 解决 IE8 不支持 :last-child
-				form.find('.fields:last-child, .field:last-child').addClass('last');
-				
-				// 解决 label 高度随 textarea 动态撑开 
-				form.find('span.textbox').each(function() {
-					var height = $(this).height()+5;
-					$(this).parent().find('>label').css({
-						'line-height': height+'px'
-					});
-				});
-			}
-		}
-	});
-	
-	return Page;
+    this.beforeLoad = function (data) {
+      return true;
+    };
+
+    // 加载数据
+    this.load = function (panel, data) {
+    };
+
+    // 提交
+    this.submit = function (panel, data, closeCallback) {
+    };
+
+    // 注销
+    this.onClose = function (table, data) {
+      return data
+    };
+
+    // 注销前事件
+    this.onBeforeClose = function (panel) {
+    };
+
+    this.$afterEvents = {};
+
+    this.$getRootLayout = function(panel) {
+      panel = panel || this.panel;
+
+      var layout = panel;
+
+      if (layout.is('.layout')) {
+        return layout;
+      }
+
+      layout = $('>.layout', layout);
+      if (layout.length) return layout;
+    };
+
+    this.$autoHeight = function(region) {
+
+      // 直接传入 panel 对象
+      if ('object' === typeof region) {
+        if (!region.panel) return;
+
+        return region.panel('resize')
+      }
+
+      var root = this.$getRootLayout();
+
+      // 没有找到 layout 直接使用 panel
+      if (!root) {
+        this.panel.panel('doLayout')
+      }
+      else {
+        region = region || 'north';
+        var panel = root.layout('panel', region);
+        panel.panel('resize', {
+          height: 'auto'
+        });
+        root.layout('resize')
+      }
+    };
+
+    /**
+     * 特殊处理打印form
+     */
+    function initPrintForm(panel) {
+      var form = panel.find('form.print');
+
+      if (form[0]) {
+        // 解决 IE8 不支持 :last-child
+        form.find('.fields:last-child, .field:last-child').addClass('last');
+
+        // 解决 label 高度随 textarea 动态撑开
+        form.find('span.textbox').each(function () {
+          var height = $(this).height() + 5;
+          $(this).parent().find('>label').css({
+            'line-height': height + 'px'
+          });
+        });
+      }
+    }
+  });
+
+  return Page;
 });
