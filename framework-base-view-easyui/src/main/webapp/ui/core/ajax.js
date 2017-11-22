a219173da3a4b1ed4d4bca74b3ec6de294f304cc
zk <zk@centit.com>
2017-11-22 18:30:04 +0800

    添加 ajax payload 模式

--- framework-base-view-easyui/src/main/webapp/ui/core/ajax.js
+++ framework-base-view-easyui/src/main/webapp/ui/core/ajax.js
@@ -1,7 +1,7 @@
 define(function(require) {
 	/**
 	 * ES6异步写法
-	 * 
+	 *
 	 * 参考：
 	 * 	http://www.html5rocks.com/zh/tutorials/es6/promises/
 	 * 	https://github.com/jakearchibald/es6-promise
@@ -9,16 +9,16 @@ define(function(require) {
 	require('plugins/es6-promise.min');
 	var $ = require('jquery');
 	var Cache = require('core/cache');
-	
+
 	$.ajaxSetup({
 		contentType: "application/x-www-form-urlencoded; charset=utf-8"
     });
-	
+
 	/**
 	 * ES6异步写法重新封装ajax请求
 	 */
 	var Ajax = {};
-	
+
 	/**
 	 * Promise封装$.ajax
 	 */
@@ -26,35 +26,41 @@ define(function(require) {
 
 
 		var csrf = Cache.get('csrf') ? Cache.get('csrf')['token'] : null;
-		
+
 		options = $.extend({
 			dataType: 'json'
 		}, options);
-		
+
 		options.data = $.extend({}, options.data, {
 			_r: Math.random(),
 			_csrf: csrf
 		});
-		var params = $.param(options.data, false);
-		params = decodeURIComponent(params);
-		
-		// 转换 optDefs[0][optCode]=1 为 optDefs[0].optCode=1
-		params = params.replace(/\[([_$a-z]\w*?)\]/g, '.$1');
-		
-		// 转换 test[]=1&test[]=2 为 test=1&test=2
-		params = params.replace(/(\[\])/g, "");
-		
-		// 中文转码
-		params = encodeURI(params);
 
-		options.data = params;
-	
+		if (options.payload) {
+		  options.contentType = 'application/json';
+      options.data = JSON.stringify(options.data);
+    } else {
+      var params = $.param(options.data, false);
+      params = decodeURIComponent(params);
+
+      // 转换 optDefs[0][optCode]=1 为 optDefs[0].optCode=1
+      params = params.replace(/\[([_$a-z]\w*?)\]/g, '.$1');
+
+      // 转换 test[]=1&test[]=2 为 test=1&test=2
+      params = params.replace(/(\[\])/g, "");
+
+      // 中文转码
+      params = encodeURI(params);
+
+      options.data = params;
+    }
+
 		return new Promise(function(resole, reject) {
 			options = $.extend(true, {}, options, {
 				success: function(data, textStatus, jqXHR) {
 					resole(data, textStatus, jqXHR);
 				},
-				
+
 				error: function(result) {
 					reject(result);
 				}
@@ -77,7 +83,7 @@ define(function(require) {
 
 		return Ajax.ajax(url, options, true);
 	};
-	
+
 	/**
 	 * Promise封装$.getJSON
 	 */
@@ -89,6 +95,6 @@ define(function(require) {
 
 		return Ajax.ajax(url, options);
 	};
-	
+
 	return Ajax;
 });
